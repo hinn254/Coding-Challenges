e2a49894bcd0da4976926adbad946933c0700258
hinn254 <bennyhinnotieno@gmail.com>
2022-05-16 07:22:03 +0300

    bubble sort v3

--- bubblesort.js
+++ bubblesort.js
@@ -11,4 +11,58 @@ function bubbleSort(nums) {
       }
     }
   } while (swapped);
+  console.log(nums);
 }
+
+function bblSort(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    // Last i elements are already in place
+    for (var j = 0; j < arr.length - i - 1; j++) {
+      // Checking if the item at present iteration
+      // is greater than the next iteration
+      if (arr[j] > arr[j + 1]) {
+        // If the condition is true then swap them
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  console.log(arr);
+}
+
+// Optimized implementation of bubble sort Algorithm
+
+function bubbleSort(arr) {
+  var i, j;
+  var len = arr.length;
+
+  var isSwapped = false;
+
+  for (i = 0; i < len; i++) {
+    isSwapped = false;
+
+    for (j = 0; j < len; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        isSwapped = true;
+      }
+    }
+
+    // IF no two elements were swapped by inner loop, then break
+
+    if (!isSwapped) {
+      break;
+    }
+  }
+
+  // Print the array
+  console.log(arr);
+}
+
+var arr = [243, 45, 23, 356, 3, 5346, 35, 5];
+
+// calling the bubbleSort Function
+bblSort(arr);
